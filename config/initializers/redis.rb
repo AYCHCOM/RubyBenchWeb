939c03d27992ce51383ecaf992a5f7078aff0c08
Jerry Liu <jerry.liu@shopify.com>
2017-02-09 16:56:28 -0500

    update redis url to use REDISCLOUD_URL if available

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,2 @@
-$redis = Redis.new(port: 6379)
+$redis = ENV['REDISCLOUD_URL'] ? Redis.new(url: ENV['REDISCLOUD_URL']) : Redis.new(port: 6379)
 RubyBenchWeb::Application.config.cache_store = :redis_store, $redis.client.id
