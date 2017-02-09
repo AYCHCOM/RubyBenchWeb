fefe04e589368ead99ff3a2613b9ae4cd5545001
Jerry Liu <jerry.liu@shopify.com>
2017-02-09 14:07:46 -0500

    change UNICORN_PORT -> PORT

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -5,7 +5,7 @@ working_directory = app_path
 
 worker_processes (ENV["UNICORN_WORKERS"] || 3).to_i
 
-listen (ENV["UNICORN_PORT"] || 3000).to_i
+listen (ENV["PORT"] || 3000).to_i
 
 #if ENV["RAILS_ENV"] == "production"
   #stderr_path "/shared/log/rails/unicorn.stderr.log"
